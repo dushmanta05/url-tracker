592c8a7c67c16d38220777b84d09778bf0fccce7
Diilu Behera <imdilub@gmail.com>
2022-09-14 13:02:00 +0530

    Chrome Tab API used to get current URL

--- index.js
+++ index.js
@@ -1,35 +1,37 @@
 let savedUrl = [];
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el");
+const urlFromLocalStorage = JSON.parse(localStorage.getItem("savedUrl"));
+const inputBtn = document.getElementById("input-btn");
+const tabBtn = document.getElementById("tab-btn");
+const deleteBtn = document.getElementById("delete-btn");
 
-
-let render = (url) => {let listItems = "";
-
+let render = (url) => {
+    let listItems = "";
     for (let i = 0; i < url.length; i++) {
         listItems += `<li><a href="${url[i]}" target= "_blank">${url[i]}</a></li>`
     }
-
     ulEl.innerHTML = listItems;
 }
-
-const urlFromLocalStorage = JSON.parse(localStorage.getItem("savedUrl"))
-
 if (urlFromLocalStorage){
     savedUrl = urlFromLocalStorage;
     render(savedUrl);
 }
-
-const inputBtn = document.getElementById("input-btn");
 inputBtn.addEventListener("click", () => {
     savedUrl.push(inputEl.value);
     inputEl.value = ""
 
     localStorage.setItem("savedUrl", JSON.stringify(savedUrl))
-    render(savedUrl);
-    
+    render(savedUrl);    
 })
+tabBtn.addEventListener("click", () => {
 
-const deleteBtn = document.getElementById("delete-btn")
+    chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+        savedUrl.push(tabs[0].url);
+        localStorage.setItem("savedUrl", JSON.stringify(savedUrl));
+        render(savedUrl);
+    })
+})
 deleteBtn.addEventListener("dblclick", () => {
     localStorage.clear()
     savedUrl = [];
